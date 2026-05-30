ea7d9ee84f9dbf23c98b64324a5c1bcfc8f8ff9f
Andrey S <andrey.n.solyanik@gmail.com>
2026-05-30 21:33:07 +0500

    Добавлены компоненты Input, Password и Button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Button } from './shared/components/button/button';
+import { InputField } from './shared/components/input-field/input-field';
+import { PasswordInput } from './shared/components/password-input/password-input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Button, InputField, PasswordInput],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Famiry');
+
+  data: string = ''
+  password: string = ''
+
+  showData(event: string): void {
+    this.data = event;
+  }
+
+  showPassword(event: string): void {
+    this.password = event;
+  }
 }
